763b788c7a60695acede1d1306b7f6c234d94bfb
larrabyte <larrabyte@protonmail.ch>
2022-10-15 20:18:37 +1100

    Add GTK4 as a dependency

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use gtk4::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let app = gtk4::Application::builder()
+        .application_id("dev.larrabyte.phonoscope")
+        .build();
+
+    app.connect_activate(|app| {
+        // Create the main window.
+        let window = gtk4::ApplicationWindow::builder()
+            .application(app)
+            .default_width(640)
+            .default_height(480)
+            .title("Phonoscope")
+            .build();
+
+        window.show();
+    });
+
+    app.run();
 }
