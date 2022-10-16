14c0c8d214367070b6538eff0bdd3c47fe6bee69
larrabyte <larrabyte@protonmail.ch>
2022-10-16 21:16:21 +1100

    Add rudamentary lyrics fetching

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,11 @@ struct Arguments {
 
     /// Name of the player to target.
     #[arg(short, long)]
-    player: Option<String>
+    player: Option<String>,
+
+    /// The directory where lyrics are stored.
+    #[arg(short, long)]
+    source: String
 }
 
 fn main() {
@@ -51,9 +55,16 @@ fn main() {
         let unique_name = player.unique_name();
         println!("Using player: {identity} (bus name {unique_name})");
 
+        let list = gtk4::ListBox::builder()
+            .margin_top(12)
+            .margin_bottom(12)
+            .margin_start(12)
+            .margin_end(12)
+            .build();
+
         let title = gtk4::Label::builder()
             .label("No song currently playing.")
-            .selectable(true)
+            .selectable(false)
             .single_line_mode(true)
             .wrap(false)
             .margin_top(12)
@@ -62,20 +73,61 @@ fn main() {
             .margin_end(12)
             .build();
 
+        let lyrics = gtk4::Label::builder()
+            .label("LYRICS GO HERE")
+            .selectable(false)
+            .single_line_mode(false)
+            .wrap(false)
+            .margin_top(12)
+            .margin_bottom(12)
+            .margin_start(12)
+            .margin_end(12)
+            .build();
+
+        list.append(&title);
+        list.append(&lyrics);
+
         let window = gtk4::ApplicationWindow::builder()
             .application(app)
             .default_width(128)
             .default_height(72)
             .title("Phonoscope")
-            .child(&title)
+            .child(&list)
             .build();
 
         // TODO: Fix intermittent failure to get track metadata when rapidly switching tracks.
-        let refresh_interval = std::time::Duration::from_millis(100);
+        let refresh_interval = std::time::Duration::from_millis(1000);
+        let source_directory = std::path::PathBuf::from(&args.source);
+
         glib::timeout_add_local(refresh_interval, move || {
             let metadata = player.get_metadata().unwrap();
-            let name = metadata.title().unwrap_or("No song currently playing.");
-            title.set_text(name);
+            let name = metadata.title();
+
+            if let Some(n) = name {
+                title.set_text(n);
+
+                // TODO: Is doing I/O here actually bad?
+                let mut path = source_directory.clone();
+                let tail = n.to_owned() + ".lyrics";
+                path.push(tail);
+
+                let data = std::fs::read_to_string(&path);
+
+                if let std::result::Result::Ok(text) = data {
+                    lyrics.set_text(&text);
+                }
+
+                else {
+                    let message = format!("Unable to read lyrics: {path:?}");
+                    lyrics.set_text(&message);
+                }
+            }
+
+            else {
+                title.set_text("No song currently playing.");
+                lyrics.set_text("No lyrics available.");
+            }
+
             glib::Continue(true)
         });
 
